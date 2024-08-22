a263ead78702d1f38244a2f88fa7d3ce9c97984f
cavini <arthurcavini@hotmail.com>
2024-08-22 08:58:41 -0300

    fix: typo on updateRestaurant validation

--- src/controllers/MyRestaurantController.ts
+++ src/controllers/MyRestaurantController.ts
@@ -58,7 +58,7 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
     restaurant.deliveryPrice = req.body.deliveryPrice;
     restaurant.estimatedDeliveryTime = req.body.estimatedDeliveryTime;
     restaurant.cuisines = req.body.cuisines;
-    restaurant.menuItems = req.body.estimatedDeliveryTime;
+    restaurant.menuItems = req.body.menuItems;
     restaurant.lastUpdated = new Date();
 
     if (req.file) {
