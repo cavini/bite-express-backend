b48189414123c9f4add3de3d8e5ef4e9c2eb5f57
cavini <arthurcavini@hotmail.com>
2024-08-25 14:49:06 -0300

    feat: stripe integration

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
 import cloudinary from "cloudinary";
+import { CheckoutSessionRequest } from "../controllers/OrderController";
+import { MenuItemType } from "../models/restaurant";
+import Stripe from "stripe";
 
 export const uploadImage = async (file: Express.Multer.File) => {
   const image = file;
@@ -7,3 +10,31 @@ export const uploadImage = async (file: Express.Multer.File) => {
   const uploadResponse = await cloudinary.v2.uploader.upload(dataURI)
   return uploadResponse.url
 }
+
+export const createLineItems = (checkoutSessionRequest: CheckoutSessionRequest, menuItems: MenuItemType[]) => {
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find((item) => item._id.toString() === cartItem.menuItemId.toString())
+
+    if (!menuItem) {
+      throw new Error(`Menu item not found: ${cartItem.name} ${cartItem.menuItemId}`)
+    }
+
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: "usd",
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name,
+        },
+      },
+      quantity: parseInt(cartItem.quantity),
+
+    }
+
+    return line_item;
+  })
+
+  return lineItems
+}
+
+
