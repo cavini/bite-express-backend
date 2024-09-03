c0ca35aee570bb4b7db149fa3cba74d7fb8110ca
cavini <arthurcavini@hotmail.com>
2024-09-03 19:02:19 -0300

    chore: reverting it back to 10 mins

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -7,7 +7,7 @@ import https from "https";
 // running for recruiters and other people that might want to check out this project.
 
 const backendUrl = `${process.env.BACKEND_PROD_URL}/health`;
-export const job = new CronJob("*/1 * * * *", function () {
+export const job = new CronJob("*/10 * * * *", function () {
   console.log("Restarting server");
   https
     .get(backendUrl, (res: IncomingMessage) => {
